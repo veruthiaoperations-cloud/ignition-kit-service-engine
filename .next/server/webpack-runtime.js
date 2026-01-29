151aa4df7fc23046557608d9977c7876ba44735c
veruthiaoperations-cloud <veruthia.operations@gmail.com>
2026-01-28 23:49:36 -0600

    Updated app-build-manifest.json

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("0f61c62424f0da06")
+/******/ 		__webpack_require__.h = () => ("9f1a1d5c2c186154")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fd5e2a047470533d"; }
+/******/ 		__webpack_require__.h = function() { return "ec2e80b8fc8c2147"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
